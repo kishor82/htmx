225f5a73c6f4fbdbdb2a32781b5717993ab8ac4e
Kishor Rathva <kishorrathva8298@gmail.com>
2024-01-10 00:50:38 +0530

    Added user api

--- server.js
+++ server.js
@@ -11,6 +11,26 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Handle GET request to fetch users
+
+app.get("/users", async (req, res) => {
+  // const users = [
+  //   { id: 1, name: "Kishor Rathva" },
+  //   { id: 2, name: "Elon Musk" },
+  //   { id: 3, name: "Modi" },
+  // ];
+  const limit = +req.query.limit || 10;
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
+  const users = await response.json();
+
+  res.send(`
+  <h1 class="text-2xl font-bold my-4">Users</h1>
+  <ul>
+    ${users.map((user) => `<li>${user.name}</li>`).join("")}
+  </ul>
+  `);
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
