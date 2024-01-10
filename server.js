79373074db08c9a988ea2c89599699558f9de7da
Kishor Rathva <kishorrathva8298@gmail.com>
2024-01-11 02:23:12 +0530

    Added search and validation with htmx.

--- server.js
+++ server.js
@@ -63,6 +63,80 @@ app.get("/get-temperature", (req, res) => {
   res.send(currentTemperature.toFixed(1) + "°C");
 });
 
+// Handle Post request for user serach from jsonplaceholder
+app.post("/search/api", async (req, res) => {
+  const searchTerm = req.body.search.toLowerCase();
+
+  if (!searchTerm) {
+    return res.send(`
+    <tr></tr>
+    `);
+  }
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const contacts = await response.json();
+
+  const searchResults = contacts.filter((contact) => {
+    const name = contact.name.toLowerCase();
+    const email = contact.email.toLowerCase();
+
+    return name.includes(searchTerm) || email.includes(searchTerm);
+  });
+
+  const searchResultHtml = searchResults
+    .map(
+      (contact) => `
+    <tr>
+      <td><div class="my-4 p-2">${contact.name}</div></td>
+      <td><div class="my-4 p-2">${contact.email}</div></td>
+    </tr>
+  `
+    )
+    .join("");
+
+  res.send(searchResultHtml);
+});
+const getEmailTemplate = (value, info) => {
+  return `<div class="mb-4" hx-target="this" hx-swap="outerHTML">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+          >Email Address</label
+        >
+        <input
+          name="email"
+          hx-post="/contact/email"
+          class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+          type="email"
+          id="email"
+          value="${value}"
+          required
+        />
+        <div class="${info.class}">${info.message}</div>
+      </div>
+  `;
+};
+// Handle POST request for email validation
+
+app.post("/contact/email", (req, res) => {
+  const submittedEmail = req.body.email;
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+  const isValid = {
+    message: "That email is valid",
+    class: "text-green-700",
+  };
+
+  const inValid = {
+    message: "Please eneter a valid email address",
+    class: "text-red-700",
+  };
+
+  if (!emailRegex.test(submittedEmail)) {
+    return res.send(getEmailTemplate(submittedEmail, inValid));
+  } else {
+    return res.send(getEmailTemplate(submittedEmail, isValid));
+  }
+});
+
 // Start the server
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
